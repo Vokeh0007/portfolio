afc52ac6330903a11d90b1620f30075aa042366d
Your GitHub Username <your-email@example.com>
2024-11-07 21:54:28 +0300

    first version

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".multiple-text", {
+  strings: ["Frontend developer.", "Youtuber.", "Freelancer.", "Designer.", "Developer.", "Photographer.", "Traveller."],
+  typeSpeed: 30,
+  backSpeed: 30,
+  backDelay: 1000,
+  startDelay: 1000,
+  loop: true
+});
